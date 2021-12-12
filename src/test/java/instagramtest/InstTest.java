1911b3535443195ad53c281883324135a3b4f449
Oleh Zaytsev <43407498+olegzaycev@users.noreply.github.com>
2021-12-12 22:30:36 +0200

    Add files via upload

--- src/test/java/instagramtest/InstTest.java
+++ src/test/java/instagramtest/InstTest.java
@@ -1,16 +1,15 @@
 package instagramtest;
 
-import Action.FollowUser;
-import Action.LoginPage;
-import Action.SearchPeople;
-import Action.SendMessage;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import Action.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 import dataconfig.Data;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 
 
 public class InstTest {
@@ -19,47 +18,74 @@ public class InstTest {
     public static SearchPeople searchPeople;
     public static FollowUser followUser;
     public static SendMessage sendMessage;
+    public static LogOut logOut;
+    public static WebDriver driver;
 
 
+    @DataProvider(name = "LoginToInstProvider")
+    public Object[][] getData() {
+        Object data [][] =
+                {
+                    {
+                        Data.getProperty("login"),
+                        Data.getProperty("password"),
+                        Data.getProperty("fisrtusersearch")
+                    },
+                    {
+                        Data.getProperty("login"),
+                        Data.getProperty("password"),
+                        Data.getProperty("secondusersearch")
+                    }
 
 
-
+                };
+        return data;
+    }
 
     @BeforeClass
     public static void SetUp()
     {
         System.setProperty("webdriver.chrome.driver", Data.getProperty("chromepath"));
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         ActLogPage = new LoginPage(driver);
         searchPeople = new SearchPeople(driver);
         followUser = new FollowUser(driver);
         sendMessage = new SendMessage(driver);
+        logOut = new LogOut(driver);
 
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(Data.getProperty("webapplication"));
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+
 
 
 
 
 
     }
-    @Test
-    public void Scene(){
-        ActLogPage.ActionInputLogin(Data.getProperty("login"));
-        ActLogPage.ActionInputPassword(Data.getProperty("password"));
+
+    @Test(dataProvider = "LoginToInstProvider")
+    public void Scene(String login, String password, String search)
+    {
+        ActLogPage.ActionInputLogin(login);
+        ActLogPage.ActionInputPassword(password);
         ActLogPage.ActionPressLogButton();
-        searchPeople.InputTextToSearch(Data.getProperty("usersearch"));
+        searchPeople.InputTextToSearch(search);
         searchPeople.ClickOnUser();
         followUser.Follow();
         sendMessage.ClickOnMail();
         sendMessage.SendMessagetoUser("Hello World!!!");
+        //sendMessage.ClickOnSend();
+        //logOut.ClickIcon();
+        //logOut.ClickToExit();
+        //driver.quit();
     }
-    @Test
+    @AfterClass
     public void End()
     {
-
+    driver.quit();
 
     }
 
